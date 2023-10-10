cb09faefffb3f18085016b9e54fc0ca7e6b5ac41
Dhanasrisalla <salladhanasri@gmail.com>
2023-10-09 23:06:38 -0400

    initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt = findViewById(R.id.text);
+    Button  btn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.text);
+        btn = findViewById(R.id.Random);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int val = random.nextInt(15);
+                txt.setText(Integer.toString(val));
+            }
+        });
+
+
+    }
+}
